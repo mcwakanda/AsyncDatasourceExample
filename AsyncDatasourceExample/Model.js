06c400679f2d7decbe09304d06091e4782a7ad2d
Greg McCarvell <greg@gregmccarvell.com>
2013-08-19 20:30:12 -0400

    Initial commit

---
+++ AsyncDatasourceExample/Model.js
@@ -0,0 +1,16 @@
+﻿
+guidedModel =// @startlock
+{
+	DataClass1 :
+	{
+		methods :
+		{// @endlock
+			serverMethod:function(clientMessage)
+			{// @lock
+				debugger;
+				//setting the message from the server!!
+				return clientMessage + " & " + "This message is from the server";
+			}// @startlock
+		}
+	}
+};// @endlock
---
+++ AsyncDatasourceExample/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,23 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+function alertMessage(event){
+	alert(event.result); //event.result is the value from the server
+}
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		debugger;
+		sources.dataClass1.serverMethod({
+			onSuccess: alertMessage
+		}, "Message from Client");
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
